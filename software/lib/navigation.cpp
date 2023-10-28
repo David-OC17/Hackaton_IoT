d2ba6ff58f05afa586690ffe3d0c5a75f5d82d0f
David-OC17 <daveoc01@icloud.com>
2023-10-28 15:11:56 -0600

    Added connection to Arduino Cloud

--- software/lib/navigation.cpp
+++ software/lib/navigation.cpp
@@ -12,69 +12,109 @@
 #include "./motor_control.cpp"
 #include "./sensor_readings.cpp"
 
-void exploreLeft(){
+bool exploreLeft(motor &motorA, motor &motorB, ultrasonicSensors &U_sensors, int minDistanceFront){
+    rotate90('L', motorA, motorB);
+    readUltrasonic(U_sensors.right);
 
+    int originalDistance = U_sensors.right.distance;
+    readUltrasonic(U_sensors.center);
+    
+    // Must check right sensor for opening and center for space
+    while (originalDistance == U_sensors.right.distance && U_sensors.center.distance > minDistanceFront){
+        // Move forward a little
+        moveForward(255, motorA, motorB);
+        delay(250);
+        stop(motorA, motorB);
+
+        // Get new reading up front and to the side
+        readUltrasonic(U_sensors.center);
+        readUltrasonic(U_sensors.right);
+    }
+    // When it exists, check if we have found an opening or we face a limit
+    // If we found an opening, then true
+    // If we face a limit, then false
+    bool nextAction = originalDistance != U_sensors.right.distance || U_sensors.center.distance < minDistanceFront ? true : false;
+
+    if(nextAction){
+        moveHalfRobotLength(motorA, motorB);
+        rotate90('R', motorA, motorB); // Return to center
+        // Exit the function, we have found opening and centered to it
+        return true;
+    }else{
+        // We must rotate 180 degrees and explore the other way
+        rotate180(motorA, motorB);
+        return false;
+    }
 };
 
-void lookForSideOpening(char firstRotation, motor &motorA, motor &motorB, ultrasonicSensors &U_sensors, int minDistanceFront){
+bool exploreRight(motor &motorA, motor &motorB, ultrasonicSensors &U_sensors, int minDistanceFront){
+    rotate90('R', motorA, motorB);
+    readUltrasonic(U_sensors.right);
+
+    int originalDistance = U_sensors.left.distance;
+    readUltrasonic(U_sensors.center);
+    
+    // Must check right sensor for opening and center for space
+    while (originalDistance == U_sensors.left.distance && U_sensors.center.distance > minDistanceFront){
+        // Move forward a little
+        moveForward(255, motorA, motorB);
+        delay(250);
+        stop(motorA, motorB);
+
+        // Get new reading up front and to the side
+        readUltrasonic(U_sensors.center);
+        readUltrasonic(U_sensors.left);
+    }
+    // When it exists, check if we have found an opening or we face a limit
+    // If we found an opening, then true
+    // If we face a limit, then false
+    bool nextAction = originalDistance != U_sensors.left.distance || U_sensors.center.distance < minDistanceFront ? true : false;
+
+    if(nextAction){
+        moveHalfRobotLength(motorA, motorB);
+        rotate90('L', motorA, motorB); // Return to center
+        // Exit the function, we have found opening and centered to it
+        return true;
+    }else{
+        // We must rotate 180 degrees and explore the other way
+        rotate180(motorA, motorB);
+        return false;  // Return to call the explore on the other side of the maze
+    }
+};
+
+// Return true if any of the explorations where successful, else return false
+bool lookForSideOpening(char firstRotation, motor &motorA, motor &motorB, ultrasonicSensors &U_sensors, int minDistanceFront){
     // Rotates to the desired side, explores until it finds a side opening and returns the robot to center
     // If it does not find an opening on that side, rotates 180 and explores to the other end
     // If no opening is found, an error is printed to serial monitor
 
-    if(firstRotation == 'L'){
-        rotate90(firstRotation, motorA, motorB);
-        readUltrasonic(U_sensors.right);
+    bool exploreDirections[2] = {false, false}; // Have not explored either way --> {left, right}
+    bool explorationSuccess[2] = {false, false}; // Explore right and left report if their search was successful
 
-        int originalDistance = firstRotation == 'L' || firstRotation == 'R' ? U_sensors.right.distance : U_sensors.left.distance;
-        readUltrasonic(U_sensors.center);
-        
-        // Must check right sensor for opening and center for space
-        while (originalDistance == U_sensors.right.distance && U_sensors.center.distance > minDistanceFront){
-            // Move forward a little
-            moveForward(255, motorA, motorB);
-            delay(250);
-            stop(motorA, motorB);
-
-            // Get new reading up front and to the side
-            readUltrasonic(U_sensors.center);
-            readUltrasonic(U_sensors.right);
-        }
-        // When it exists, check if we have found an opening or we face a limit
-        // If we found an opening, then true
-        // If we face a limit, then false
-        bool nextAction = originalDistance != U_sensors.right.distance || U_sensors.center.distance < minDistanceFront ? true : false;
-
-        if(nextAction){
-            moveHalfRobotLength(motorA, motorB);
-            rotate90('R', motorA, motorB); // Return to center
-            // Exit the function, we have found opening and centered to it
+    while(!exploreDirections[0] || !exploreDirections[1]){
+        if(firstRotation == 'L' && !exploreDirections[0]){
+            explorationSuccess[0] =  exploreLeft(motorA, motorB, U_sensors, minDistanceFront);
+            exploreDirections[0] = true;
+        }else if(!exploreDirections[1]){
+            explorationSuccess[1] = exploreLeft(motorA, motorB, U_sensors, minDistanceFront);
+            exploreDirections[1] = true;
         }else{
-            // We must rotate 180 degrees and explore the other way
-            rotate180(motorA, motorB);
+            stop(motorA, motorB);
+            Serial.print("Explored both sided!");
         }
-        
-    }
-    
-    
-    
-    
-    else if(firstRotation == 'R'){
-        rotate90(firstRotation, motorA, motorB);
-        readUltrasonic(U_sensors.left);
-    }else{
-        Serial.print("Not a valid rotation direction.");
     }
 
-    int originalDistance = firstRotation == 'L' || firstRotation == 'R' ? U_sensors.right.distance : U_sensors.left.distance;
-    readUltrasonic(U_sensors.center);
-
-    // While the front is clear and we have not found an opening, continue exploring
-    while (originalDistance == U_sensor.distance)
-    {
-        /* code */
+    // Check if any of the two explorations was successful
+    if (!explorationSuccess[0] && !explorationSuccess[1]){
+        Serial.print("No valid path found!");
+    }else if(explorationSuccess[0]){
+        Serial.print("Left exploration successful! Continuining with path.");
+        return true;
+    }else if(explorationSuccess[1]){
+        Serial.print("Right exploration successful! Continuining with path.");
+        return true;
+    }else {
+        Serial.print("Error after exploration.");
+        return false;
     }
-    
-
-
-    
-}
+}    
--- software/src/main.cpp
+++ software/src/main.cpp
@@ -1,7 +1,29 @@
+/**************************************************************************
+ * Main script
+ * Autor: David Ortiz Cota
+ * Fecha: 28/10/2023
+ * 
+ * Descripcion:
+ * Este archivo contiene la implementación centralizada para la navegación
+ * del robot en el laberinto. 
+ **************************************************************************/
+
 #include <Arduino.h>
 #include "../lib/motor_control.cpp"
 #include "../lib/sensor_readings.cpp"
 #include "../lib/navigation.cpp"
+#include "../lib/thingProperties.h"
+
+//////////////////// Cloud connection notes ////////////////////
+/*
+The variable being passed to the cloud is:
+    String exploreStatus;
+This variables gives the report of the state/status of the robot in its navigation
+process. The variable is updated and sent when it changes. Change the variable
+'explreStatus' and wait to the next execution of 'loop' to see changes.
+
+To be able to connect to Arduino Cloud, the file 'thingProperties.h' is included.
+*/
 
 //////////////////// Pin configuration ////////////////////
 
@@ -29,8 +51,11 @@ ultrasonic U_sensorLeft(0, 0, 5);
 ultrasonic U_sensorFront(0, 0, 6);
 ultrasonic U_sensorRight(0, 0, 7);
 
-ultrasonicSensors(U_sensorLeft, U_sensorFront, U_sensorRight);
+ultrasonicSensors U_sensors(U_sensorLeft, U_sensorFront, U_sensorRight);
 
+// Initialize variables for the control and decision making
+int minDistanceFront = 50; // Milimiters
+bool moveForwardLock = false;
 
 void setup() {
     // Set pin modes for motor A
@@ -51,6 +76,20 @@ void setup() {
     digitalWrite(motorB.in2, LOW);
   
     Serial.begin(115200); // Check for the particual baud rate of your microcontroller
+    delay(1500);
+
+    // Connect to Arduino IoT Cloud
+    ArduinoCloud.begin(ArduinoIoTPreferredConnection);
+    
+    /*
+      The following function allows you to obtain more information
+      related to the state of network and IoT Cloud connection and errors
+      the higher number the more granular information you’ll get.
+      The default is 0 (only errors).
+      Maximum is 4
+  */
+    // setDebugMessageLevel(2);
+    // ArduinoCloud.printDebugInfo();
 }
 
 
@@ -67,35 +106,40 @@ void setup() {
       B.2.b If opening not found, rotate 180 degrees and look for new opening
 */
 
-void loop() {
-  // Initialize variables for the control and decision making
-  int minDistanceFront = 50; // Milimiters
-  bool moveForwardLock = false;
+void loop(int minDistanceFront, bool moveForwardLock) {
+  ArduinoCloud.update(); // Updates the variables and sends them to Arduino Cloud
 
-  int lightSensor = maxIntensityLight(L_sensors, L_readings);
+  if(!moveForwardLock){
+    int lightSensor = maxIntensityLight(L_sensors, L_readings);
 
-  // Initial correction to look forward - check the time needed for the robot to rotate 90 degrees
-  if (lightSensor == 0) {
-    // Right sensor has the highest intensity
-    rotate90('L', motorA, motorB);
+    // Initial correction to look forward - check the time needed for the robot to rotate 90 degrees
+    if (lightSensor == 0) {
+      // Right sensor has the highest intensity
+      rotate90('L', motorA, motorB);
+    }
+    else if (lightSensor == 2) {
+      // Left sensor has the highest intensity (or they are equal)
+      rotate90('R', motorA, motorB);
+    }// else the robot is already centered
+
+    // Now, you are facing forward. Check the path to the front.
+    readUltrasonic(U_sensorFront);
+    while(U_sensorFront.distance > minDistanceFront){
+      // Path is clear, move forward
+        moveForward(255, motorA, motorB);
+        delay(250); // Time to move forward
+        stop(motorA, motorB);
+    }
+
+    // The path is no longer clear, rotate to look for new path
+    readUltrasonic(U_sensorLeft);
+    readUltrasonic(U_sensorRight);
+    char rotate = U_sensorRight.distance >= U_sensorLeft.distance ? 'L' : 'R'; // Determine which side to explore first
+    bool moveForwardLock =  lookForSideOpening(rotate, motorA, motorB, U_sensors, minDistanceFront);
+
+    // We take the lock and use it to determine if we should stop the robot completelly or to continue with the next loop iteration
+  }else{ // Do not move further if no path was found
+    stop(motorA, motorB);
+    Serial.print("Error after exploration, no path found.");
   }
-  else if (lightSensor == 2) {
-    // Left sensor has the highest intensity (or they are equal)
-    rotate90('R', motorA, motorB);
-  }// else the robot is already centered
-
-  // Now, you are facing forward. Check the path to the front.
-  readUltrasonic(U_sensorFront);
-  while(U_sensorFront.distance > minDistanceFront){
-    // Path is clear, move forward
-      moveForward(255, motorA, motorB);
-      delay(250); // Time to move forward
-      stop(motorA, motorB);
-  }
-  // The path is no longer clear, rotate to look for new path
-  readUltrasonic(U_sensorLeft);
-  readUltrasonic(U_sensorRight);
-  char rotate = U_sensorRight.distance >= U_sensorLeft.distance ? 'L' : 'R'; // Determine which side to explore first
-  lookForSideOpening(rotate, motorA, motorB);
-
 }
